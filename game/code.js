f8f86b85b79113337b49bf166a5e4bff74de460c
wwwwwei <wwwwwei4@gmail.com>
2017-12-21 09:24:24 +0900

    Update code.js

--- game/code.js
+++ game/code.js
@@ -138,8 +138,7 @@ function init() {
   meshfloor.rotation.x -= Math.PI / 2;
   meshfloor.receiveShadow = true;
 
-  ambientLight = new THREE.AmbientLight(0xffffff,0.2); 
-  light.position.set(-3,6,-3);
+  ambientLight = new THREE.AmbientLight(0xffffff,0.2);
   light.castShadow = true;
   light.shadow.camera.near = 0.1;
   light.shadow.camera.far = 25;
